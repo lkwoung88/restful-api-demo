dc438ea2c493b81feced3fad61afcf2e6817693f
lkwoung88 <lkwoung88@gmail.com>
2024-08-02 00:37:32 +0900

    init

---
+++ src/test/java/hello/rest/RestfulApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
